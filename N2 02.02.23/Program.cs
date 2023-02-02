a6fe4469b0b190bf341c0eed218943e769f1445d
Velmeri <danielmikhh2gmail.com>
2023-02-02 13:19:13 +0100

    Добавьте файлы проекта.

---
+++ N2 02.02.23/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2
+{
+    static class Task
+    {
+        static readonly Random random = new Random(); // Рандом лучше не пересоздавать, так как в качестве семени используются милисекунды, которые не успевают пройти
+
+        private static void RandArr(int[] arr, int min, int max)
+        {
+            for(int i = 0; i < arr.Length; i++) arr[i] = random.Next(min, max);
+        }
+        private static void PrintArr(int[] arr)
+        {
+            Console.Write("{");
+            foreach (int a in arr) Console.Write($"{a} ");
+            Console.Write("}\n");
+        }
+        public static void N1()
+        {
+            int[] numbers = new int[10];
+            RandArr(numbers, 0, 10);
+            PrintArr(numbers);
+            int evenCount = 0;
+            int oddCount = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("Number of even elements: " + evenCount);
+            Console.WriteLine("Number of odd elements: " + oddCount);
+        }
+        public static void N2()
+        {
+            int[] numbers = new int[10];
+            RandArr(numbers, 0, 10);
+            PrintArr(numbers);
+
+            Console.Write("Enter a number: ");
+            int enteredNumber = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            foreach (int a in numbers)
+            {
+                if (a < enteredNumber)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Number of elements less than {enteredNumber}: {count}");
+        }
+        public static void N3()
+        {
+            int[] numbers = new int[10];
+            RandArr(numbers, 0, 10);
+            PrintArr(numbers);
+
+            int[] userNumbers = new int[3];
+            for (short i = 0; i < userNumbers.Length; i++)
+            {
+                Console.WriteLine($"Enter number #{i + 1}:");
+                string str = Console.ReadLine();
+                Int32.TryParse(str, out userNumbers[i]);
+            }
+
+            int count = 0;
+            for (int i = 0; i < numbers.Length - userNumbers.Length - 1; i++)
+            {
+                if (numbers[i] == userNumbers[0] && numbers[i + 1] == userNumbers[1] && numbers[i + 2] == userNumbers[2])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Count: {count}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N1:");
+            Task.N1();
+            Console.WriteLine("\n\nN2:");
+            Task.N2();
+            Console.WriteLine("\n\nN3:");
+            Task.N3();
+
+            Console.WriteLine("\nPress eny key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ N2 02.02.23/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("N2 02.02.23")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("N2 02.02.23")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("edb78f0b-19a9-4614-8df7-0cc626126562")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
